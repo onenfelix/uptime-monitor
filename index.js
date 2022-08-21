b468d6119db1182330b380e62902688890e3cbdc
Felix Onen <onenfelix90@gmail.com>
2022-08-21 11:19:18 +0300

    Added a ping router for checking the API availability

--- index.js
+++ index.js
@@ -79,8 +79,8 @@ var unifiedServer = function (req, res) {
 
 var handlers = {};
 
-handlers.sample = function (data, callback) {
-  callback(406, { name: "sample handler" });
+handlers.ping = function (data, callback) {
+  callback(200);
 };
 
 handlers.notFound = function (data, callback) {
@@ -88,5 +88,5 @@ handlers.notFound = function (data, callback) {
 };
 
 var router = {
-  sample: handlers.sample,
+  ping: handlers.ping,
 };
